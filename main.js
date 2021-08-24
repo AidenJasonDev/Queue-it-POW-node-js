51b90dddbebfa6b44deaa975ec3469ecd89e633f
Paranoia <jawsome2015@gmail.com>
2021-08-23 21:44:04 -0400

    added Solution
    Solution to Proof of Work in Node JS

---
+++ main.js
@@ -0,0 +1,29 @@
+const crypto = require('crypto')
+const utf8 = require('utf8');
+
+
+let input = "f02b931c-52f0-4507-9406-f1221678dc16"
+let zeroNum = 4
+
+function getHash(input,zeroNum) {
+    let zeros = '0'
+    while(true) {
+        zeros = zeros + '0'
+        if(zeros.length === zeroNum) {
+            break
+        }
+    }
+    let postfix = 0;
+    while(true) {
+         postfix++;
+         postfixString = postfix.toString() 
+         str = input + postfixString
+         hashEncoded = crypto.createHash('sha256').update(utf8.encode(str)).digest('hex')
+         if(hashEncoded.startsWith(zeros)) {
+             return { "postfix":postfix, "hash": hashEncoded }
+         }
+    }
+}
+
+
+console.log(getHash(input, zeroNum))
